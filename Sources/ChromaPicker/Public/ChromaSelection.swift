646e83f7430518fcab53ae859b7831506c058e45
Elyan Gutierrez <142635649+elyangutierrez@users.noreply.github.com>
2026-03-07 13:11:23 -0600

    add docC for public folder

--- Sources/ChromaPicker/Public/ChromaSelection.swift
+++ Sources/ChromaPicker/Public/ChromaSelection.swift
@@ -8,10 +8,25 @@
 import Foundation
 import SwiftUI
 
+/**
+    Uses the type that conforms to this protocol to
+    create one of the picker views as long
+    as the type is either a `Color` or `[Gradient.Stop]`.
+
+ */
+
 public protocol ChromaSelection {
     associatedtype V: View
     
+    /**
+        Makes the view based off of the type given.
+     
+        - Parameter binding: `Binding` to either `Color` or `[Gradient.Stop]`.
+     
+        - Returns: A picker view based off of the given type.
+     */
+    
     @ViewBuilder
     @MainActor
-    func makePickerView(_ binding: Binding<Self>) -> V // make the view based on the type given
+    func makePickerView(_ binding: Binding<Self>) -> V
 }
